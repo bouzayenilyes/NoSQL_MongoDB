9b529b7135a77f891826dddd6814f213bb8e61e9
NeoVimmer <ilyesbouzayen@hotmail.com>
2025-11-17 11:16:48 +0100

    refactor: update response messages and remove unused updatedAt field from Todo model

--- index.js
+++ index.js
@@ -22,12 +22,12 @@ app.use('/api/todos', require('./routes/todos'));
 
 
 app.get('/', (req, res) => {
-  res.send('Aaslema raw ee backend yekhdem...');
+  res.send('test lel backend yekhdem');
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server raw ykhdem eela l port hedha  ${PORT}`)
+  console.log(`Server yekhdem eela l ${PORT}`)
 );
